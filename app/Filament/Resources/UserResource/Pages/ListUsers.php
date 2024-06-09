8be27b2c5d209ee4d273b2318e8858be6754a1ce
AMuadib <a.muadib@gmail.com>
2024-06-09 19:13:57 +0700

    Warna dan icon tombol create

--- app/Filament/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Resources/UserResource/Pages/ListUsers.php
@@ -13,7 +13,9 @@ class ListUsers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->icon('heroicon-o-plus')
+                ->color('info'),
         ];
     }
 }
