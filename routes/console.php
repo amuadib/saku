16417bb2d90bb709cca5dded1762ec17d1621031
AMuadib <a.muadib@gmail.com>
2024-07-28 07:48:33 +0700

    Fix sintaks waktu backup

--- routes/console.php
+++ routes/console.php
@@ -19,7 +19,7 @@ Schedule::command('backup:run --only-db')->daily()->at('11:20')
         Log::info("Proses Backup berhasil dijalankan");
     });
 
-Schedule::command('backup:run')->weeklyOn(1, '2.22')
+Schedule::command('backup:run')->weeklyOn(1, '2:22')
     ->onFailure(function () {
         Log::alert("Proses Backup seluruh data gagal dijalankan");
     })
