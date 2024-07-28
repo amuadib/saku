40c3e68bd2ea2d66e23e2e6dd4ee74e80a578d93
AMuadib <a.muadib@gmail.com>
2024-07-28 07:46:48 +0700

    Tambahkan full-backup

--- routes/console.php
+++ routes/console.php
@@ -18,3 +18,11 @@ Schedule::command('backup:run --only-db')->daily()->at('11:20')
     ->onSuccess(function () {
         Log::info("Proses Backup berhasil dijalankan");
     });
+
+Schedule::command('backup:run')->weeklyOn(1, '2.22')
+    ->onFailure(function () {
+        Log::alert("Proses Backup seluruh data gagal dijalankan");
+    })
+    ->onSuccess(function () {
+        Log::info("Proses Backup seluruh data berhasil dijalankan");
+    });
