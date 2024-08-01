cfec5e51e07aab60a5b52b38aa4a454c0acfa506
AMuadib <a.muadib@gmail.com>
2024-08-01 07:43:53 +0700

    Struk penarikan Tabungan

---
+++ resources/views/cetak/contents/struk-penarikan-tabungan.blade.php
@@ -0,0 +1,28 @@
+        <h3 class="text-center">Bukti Penarikan Tabungan</h3>
+        <table style="margin-top:15px;">
+            <tr>
+                <td>
+                    {{ $data['keterangan'] }}
+                </td>
+            </tr>
+        </table>
+        <div class="divider"></div>
+        <table>
+            <tr class="font-bold">
+                <td>
+                    Penarikan
+                </td>
+                <td class="text-right">
+                    Rp
+                </td>
+                <td class="text-right">
+                    {{ format_angka($data['jumlah']) }}
+                </td>
+            </tr>
+        </table>
+
+        <div class="text-center" style="font-size: 12px; margin-top:25px;">
+            Struk ini merupakan bukti pembayaran yang sah. <br>
+            Mohon disimpan dengan baik. <br>
+            <spsn class="font-bold">Terima Kasih</spsn>
+        </div>
