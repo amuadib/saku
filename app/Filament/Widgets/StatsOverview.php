d36dfb6d9a1996270f602bd3b5e3240793485efd
AMuadib <a.muadib@gmail.com>
2024-08-05 21:07:54 +0700

    Penambahan widget Rekap transaksi

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -1,23 +1,23 @@
 <?php
 
-namespace App\Filament\Widgets;
+// namespace App\Filament\Widgets;
 
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
-use Filament\Widgets\StatsOverviewWidget\Stat;
+// use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+// use Filament\Widgets\StatsOverviewWidget\Stat;
 
-class StatsOverview extends BaseWidget
-{
-    protected static ?string $pollingInterval = null;
-    protected function getStats(): array
-    {
-        return [
-            // Stat::make('Unique views', '192.1k')
-            //     ->description('32k increase')
-            //     ->descriptionIcon('heroicon-m-arrow-trending-up', 'before')
-            //     ->chart([7, 2, 10, 3, 15, 45, 10])
-            //     ->color('success'),
-            // Stat::make('Bounce rate', '21%'),
-            // Stat::make('Average time on page', '3:12'),
-        ];
-    }
-}
+// class StatsOverview extends BaseWidget
+// {
+//     protected static ?string $pollingInterval = null;
+//     protected function getStats(): array
+//     {
+//         return [
+//             Stat::make('Unique views', '192.1k')
+//                 ->description('32k increase')
+//                 ->descriptionIcon('heroicon-m-arrow-trending-up', 'before')
+//                 ->chart([7, 2, 10, 3, 15, 45, 10])
+//                 ->color('success'),
+//             Stat::make('Bounce rate', '21%'),
+//             Stat::make('Average time on page', '3:12'),
+//         ];
+//     }
+// }
---
+++ app/Filament/Widgets/TabelTransaksi.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\RekapTransaksiHarian;
+use Filament\Tables\Table;
+use Filament\Widgets\TableWidget as BaseWidget;
+use Filament\Tables\Columns\TextColumn;
+
+class TabelTransaksi extends BaseWidget
+{
+    protected static ?string $heading = 'Rekap Transaksi Harian';
+    protected static ?string $pollingInterval = null;
+    protected static ?int $sort = 4;
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(
+                RekapTransaksiHarian::when(
+                    !auth()->user()->isAdmin(),
+                    function ($w) {
+                        $w
+                            ->join('kas', 'kas_id', '=', 'kas.id')
+                            ->where('kas.lembaga_id', auth()->user()->authable->lembaga_id);
+                    }
+                )
+                    ->orderBy('tanggal', 'desc')
+                    ->limit(7)
+            )
+            ->columns([
+                TextColumn::make('tanggal')
+                    ->date('d F Y'),
+                TextColumn::make('masuk')
+                    ->prefix('Rp ')
+                    ->numeric(thousandsSeparator: '.'),
+                TextColumn::make('keluar')
+                    ->prefix('Rp ')
+                    ->numeric(thousandsSeparator: '.'),
+                TextColumn::make('saldo')
+                    ->prefix('Rp ')
+                    ->state(fn (RekapTransaksiHarian $rekap): int => $rekap->masuk - $rekap->keluar)
+                    ->numeric(thousandsSeparator: '.'),
+            ])
+            ->emptyStateHeading('Belum ada data')
+            ->paginated(false);
+    }
+}
