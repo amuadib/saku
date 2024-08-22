5ffb5a9bb1d26cf5af0b75069094c7441edf4104
AMuadib <a.muadib@gmail.com>
2024-08-23 06:22:44 +0700

    Ubah Judul

--- app/Filament/Pages/Laporan.php
+++ app/Filament/Pages/Laporan.php
@@ -2,13 +2,11 @@
 
 namespace App\Filament\Pages;
 
-use App\Models\Kas;
-use App\Models\RekapTransaksiHarian;
 use Filament\Pages\Page;
 
 class Laporan extends Page
 {
-
+    protected static ?string $title = 'Laporan Keuangan';
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static string $view = 'filament.pages.laporan';
     protected static ?int $navigationSort = 5;
