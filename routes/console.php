8f9c50ac24e7cb44007f1219740863641eb6219a
AMuadib <a.muadib@gmail.com>
2024-08-31 21:05:54 +0700

    Update console route

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Schedule;
 use Illuminate\Support\Facades\Log;
 
-Schedule::command('backup:clean')->daily()->at('11:00')
+Schedule::command('backup:clean')
+    ->daily()
+    ->at('16:00')
     ->onFailure(function () {
         Log::alert("Proses pembersihan Backup gagal dijalankan");
     })
@@ -11,7 +13,8 @@ Schedule::command('backup:clean')->daily()->at('11:00')
         Log::info("Proses pembersihan Backup berhasil dijalankan");
     });
 
-Schedule::command('backup:run --only-db')->twiceDailyAt(11, 15, 7)
+Schedule::command('backup:run --only-db')
+    ->twiceDailyAt(11, 15, 7)
     ->onFailure(function () {
         Log::alert("Proses Backup gagal dijalankan");
     })
@@ -19,7 +22,8 @@ Schedule::command('backup:run --only-db')->twiceDailyAt(11, 15, 7)
         Log::info("Proses Backup berhasil dijalankan");
     });
 
-Schedule::command('backup:run')->weeklyOn(1, '2:22')
+Schedule::command('backup:run')
+    ->weeklyOn(1, '2:22')
     ->onFailure(function () {
         Log::alert("Proses Backup seluruh data gagal dijalankan");
     })
