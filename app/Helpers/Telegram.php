e1cccba885c3be682aa73d3bf7a6b6c552b7be3c
AMuadib <a.muadib@gmail.com>
2026-02-05 19:36:26 +0700

    ubah pengiriman Telegram menjadi asinkron

--- app/Helpers/Telegram.php
+++ app/Helpers/Telegram.php
@@ -3,15 +3,20 @@
 namespace App\Helpers;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class Telegram
 {
     public static function send($message)
     {
-        Http::post("https://api.telegram.org/bot" . env('BOT_TOKEN') . "/sendMessage", [
-            'chat_id' => env('CHAT_ID'),
-            'text'    => $message,
-            'parse_mode' => 'HTML',
-        ]);
+        try {
+            Http::timeout(2)->post("https://api.telegram.org/bot" . env('BOT_TOKEN') . "/sendMessage", [
+                'chat_id' => env('CHAT_ID'),
+                'text'    => $message,
+                'parse_mode' => 'HTML',
+            ]);
+        } catch (\Exception $e) {
+            Log::error("Gagal mengirim pesan ke Telegram. Reason: " . $e->getMessage());
+        }
     }
 }
